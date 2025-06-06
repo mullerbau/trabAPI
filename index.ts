7b74d9ccd3e7ea0fbbcffc8431ba8ca44989bc5d
ERIC MULLER BAUER <03534746066@senacrs.edu.br>
2025-06-06 10:54:19 -0300

    Trabalho 'finalizado'

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import routesAlunos from './routes/alunos'
 import routesLivros from './routes/livros'
 import routesEmprestimo from './routes/emprestimos'
+import routesEmail from './routes/alunos'
 
 dotenv.config()
 const app = express()
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use("/alunos", routesAlunos)
 app.use("/livros", routesLivros)
 app.use("/emprestimos", routesEmprestimo)
+app.use("/email", routesEmail)
 
 
 app.get('/', (req, res) => {
